4a8f9ad5c58b26b600c13a787cf345d66dbe45a7
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-03-19 00:10:43 +0800

    :+1:

--- 00escpos-usb.js
+++ 00escpos-usb.js
@@ -8,23 +8,22 @@ const device = new escpos.USB();
 // 创建打印机对象
 const printer = new escpos.Printer(device);
 
-// device.open(function(error){
-//   printer
-//     .font('a') // 选择字体
-//     .align('ct') // 居中对齐
-//     .style('bu') // 字体样式：粗体+下划线
-//     .size(1, 1) // 字体大小
-//     .text('Hello, World!') // 打印文本
-//     .cut() // 切纸
-//     .close(); // 关闭打印机连接
-// });
-
 device.open(function(error){
   printer
     .font('a') // 选择字体
     .align('ct') // 居中对齐
     .style('bu') // 字体样式：粗体+下划线
     .size(1, 1) // 字体大小
+    .text('Hello, World!') // 打印文本
     .close(); // 关闭打印机连接
 });
+
+// device.open(function(error){
+//   printer
+//     .font('a') // 选择字体
+//     .align('ct') // 居中对齐
+//     .style('bu') // 字体样式：粗体+下划线
+//     .size(1, 1) // 字体大小
+//     .close(); // 关闭打印机连接
+// });
  
